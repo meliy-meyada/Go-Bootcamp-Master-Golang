621ba3fd234ecd9f16f1104359ebbdefc67b334f
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-06-15 14:55:43 +0700

    Default Clause

---
+++ exercises/57.Switch Statement/03-default-clause/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func main(){
+	
+	city := os.Args[1]
+
+	switch city {
+	case "Paris":
+		fmt.Println("France")
+	case "Tokyo":
+		fmt.Println("Japan")
+	default:
+		fmt.Println("Where?")
+
+	}
+}
