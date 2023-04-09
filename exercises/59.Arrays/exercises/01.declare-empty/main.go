c41903c9b9bee5087d0737fd41fe32e232ac9021
Meliy <meyada.saisan@gmail.com>
2023-04-09 12:13:47 +0700

    Arrays

---
+++ exercises/59.Arrays/exercises/01.declare-empty/main.go
@@ -0,0 +1,94 @@
+// ---------------------------------------------------------
+// EXERCISE: Declare empty arrays
+//
+//  1. Declare and print the following arrays with their types:
+//
+//  1. The names of your three best friends (names array)
+//
+//  2. The distances to five different locations (distances array)
+//
+//  3. A data buffer with five bytes of capacity (data array)
+//
+//  4. Currency exchange ratios only for a single currency (ratios array)
+//
+//  5. Up/Down status of four different web servers (alives array)
+//
+//  6. A byte array that doesn't occupy memory space (zero array)
+//
+//  2. Print only the types of the same arrays.
+//
+//  3. Print only the elements of the same arrays.
+//
+// HINT
+//
+//	When printing the elements of an array, you can use the usual Printf verbs.
+//
+//	For example:
+//	  When printing a string array, you can use "%q" verb as usual.
+//
+// EXPECTED OUTPUT
+//
+//	names    : [3]string{"", "", ""}
+//	distances: [5]int{0, 0, 0, 0, 0}
+//	data     : [5]uint8{0x0, 0x0, 0x0, 0x0, 0x0}
+//	ratios   : [1]float64{0}
+//	alives   : [4]bool{false, false, false, false}
+//	zero     : [0]uint8{}
+//
+//	names    : [3]string
+//	distances: [5]int
+//	data     : [5]uint8
+//	ratios   : [1]float64
+//	alives   : [4]bool
+//	zero     : [0]uint8
+//
+//	names    : ["" "" ""]
+//	distances: [0 0 0 0 0]
+//	data     : [0 0 0 0 0]
+//	ratios   : [0.00]
+//	alives   : [false false false false]
+//	zero     : []
+//
+// ---------------------------------------------------------
+package main
+
+import "fmt"
+
+
+func main() {
+
+	// declare empty arrays
+	var (
+		names 		[3]string
+		distances	[5]int
+		data			[5]byte
+		ratios		[1]float64
+		alives		[4]bool
+		zero			[]byte
+	)
+
+	 // Print the arrays with their types
+    fmt.Printf("names    : %#v\n", names)
+    fmt.Printf("distances: %#v\n", distances)
+    fmt.Printf("data     : %#v\n", data)
+    fmt.Printf("ratios   : %#v\n", ratios)
+    fmt.Printf("alives   : %#v\n", alives)
+    fmt.Printf("zero     : %#v\n", zero)
+
+    // Print only the types of the arrays
+    fmt.Printf("names    : %T\n", names)
+    fmt.Printf("distances: %T\n", distances)
+    fmt.Printf("data     : %T\n", data)
+    fmt.Printf("ratios   : %T\n", ratios)
+    fmt.Printf("alives   : %T\n", alives)
+    fmt.Printf("zero     : %T\n", zero)
+
+    // Print only the elements of the arrays
+    fmt.Printf("names    : %q\n", names)
+    fmt.Printf("distances: %d\n", distances)
+    fmt.Printf("data     : %d\n", data)
+    fmt.Printf("ratios   : %.2f\n", ratios)
+    fmt.Printf("alives   : %t\n", alives)
+
+
+}
